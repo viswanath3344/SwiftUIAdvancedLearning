6a0ae6fa611a5ac956fbe08e4001da168b2b4655
Viswanath Reddy Ponthota (Contractor) <viswanathreddy.ponthota@cognizant.com>
2024-09-19 10:33:13 +0530

    Initial Commit

---
+++ SwiftUIAdvancedLearning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIAdvancedLearning
+//
+//  Created by Ponthota, Viswanath Reddy (Cognizant) on 19/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIAdvancedLearning/SwiftUIAdvancedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAdvancedLearningApp.swift
+//  SwiftUIAdvancedLearning
+//
+//  Created by Ponthota, Viswanath Reddy (Cognizant) on 19/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAdvancedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
